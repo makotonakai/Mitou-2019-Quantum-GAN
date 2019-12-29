7099624169fcbd98fec45832d1eb55e49da8bbe8
ntanetani <n.tanetani@gmail.com>
2019-12-29 22:54:09 +0900

    some update

--- frqi/Gates.py
+++ frqi/Gates.py
@@ -25,55 +25,47 @@ def rmcry(self, angle, bin, target, controls, anc):
         assert len(bin) == len(controls), "error"
         assert len(bin) > 5, "ERROR"
 
-        clist = []
+        clist = [controls[i] for i in range(len(bin)) if bin[i] == "0"]
+        size = len(controls)
 
-        for i in bin:
-                clist.append(int(i))
-
-        for i in range(len(clist)):
-                if clist[i] == 0:
-                        self.x(controls[-i-1])
+        self.x(clist)
         
-        for i in range(0, len(clist)-4+len(clist)%2, 2):
+        for i in range(0, size-4+size%2, 2):
                 if i == 0:
                         self.ccx(controls[i], controls[i+1], anc)
                 else:
                         self.ccx(controls[i], controls[i+1], controls[i-1])
 
-                self.x(controls[i])
-                self.x(controls[i+1])
+                self.x(controls[i:i+2])
         
-        if (len(clist)%2) == 0:
+        if size%2 == 0:
                 self.rccx(controls[-1], controls[-2], controls[-5])
         else:
                 self.cx(controls[-1], controls[-4])
         
-        for i in range(6-len(clist)%2, len(clist)+1, 2):
+        for i in range(6-size%2, size+1, 2):
                 self.rccx(controls[-i+3], controls[-i+2], controls[-i])
 
         self.mary_4(angle, target, anc, controls[0], controls[1])
 
-        for i in reversed(range(6-len(clist)%2, len(clist)+1, 2)):
+        for i in reversed(range(6-size%2, size+1, 2)):
                 self.rccx(controls[-i+3], controls[-i+2], controls[-i])
 
-        if (len(clist)%2) == 0:
+        if size%2 == 0:
                 self.rccx(controls[-1], controls[-2], controls[-5])
         else:
                 self.cx(controls[-1], controls[-4])
 
-        for i in reversed(range(0, len(clist)-4+len(clist)%2, 2)):
+        for i in reversed(range(0, size-4+size%2, 2)):
                 
-                self.x(controls[i])
-                self.x(controls[i+1])
+                self.x(controls[i:i+2])
 
                 if i == 0:
                         self.rccx(controls[i], controls[i+1], anc)
                 else:
                         self.rccx(controls[i], controls[i+1], controls[i-1])
 
-        for i in range(len(clist)):
-                if clist[i] == 0:
-                        self.x(controls[-i-1])
+        self.x(clist)
 
 QuantumCircuit.mary_4 = mary_4
 QuantumCircuit.rmcry = rmcry
