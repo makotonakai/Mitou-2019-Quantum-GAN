23c1feacc1bff712298d0aee68acce1c27b4ee9c
ntanetani <n.tanetani@gmail.com>
2019-12-30 12:59:07 +0900

    update encoder for 4*4 images

--- frqi/Gates.py
+++ frqi/Gates.py
@@ -23,22 +23,23 @@ def mary_4(self, angle, t, c0, c1, c2):
 def rmcry(self, angle, bin, target, controls, anc):
 
         assert len(bin) == len(controls), "error"
-        assert len(bin) > 4, "ERROR"
+        assert len(bin) > 3, "ERROR"
 
         clist = [controls[-i-1] for i in range(len(bin)) if bin[i] == "0"]
         size = len(controls)
 
         self.x(clist)
         
-        for i in range(0, size-4+size%2, 2):
-                if i == 0:
-                        self.ccx(controls[i], controls[i+1], anc)
-                else:
-                        self.ccx(controls[i], controls[i+1], controls[i-1])
-
+        self.ccx(controls[0], controls[1], anc)
+        self.x(controls[0:2])
+        for i in range(2, size-4+size%2, 2):
+                self.ccx(controls[i], controls[i+1], controls[i-1])
                 self.x(controls[i:i+2])
         
-        if size%2 == 0:
+        if size == 4:
+                self.cx(controls[-1], controls[-3])
+                self.cx(controls[-2], controls[-4])
+        elif size%2 == 0:
                 self.rccx(controls[-1], controls[-2], controls[-5])
         else:
                 self.cx(controls[-1], controls[-4])
@@ -50,20 +51,20 @@ def rmcry(self, angle, bin, target, controls, anc):
 
         for i in reversed(range(6-size%2, size+1, 2)):
                 self.rccx(controls[-i+3], controls[-i+2], controls[-i])
-
-        if size%2 == 0:
+        
+        if size == 4:
+                self.cx(controls[-1], controls[-3])
+                self.cx(controls[-2], controls[-4])
+        elif size%2 == 0:
                 self.rccx(controls[-1], controls[-2], controls[-5])
         else:
                 self.cx(controls[-1], controls[-4])
 
-        for i in reversed(range(0, size-4+size%2, 2)):
-                
+        for i in reversed(range(2, size-4+size%2, 2)):
                 self.x(controls[i:i+2])
-
-                if i == 0:
-                        self.rccx(controls[i], controls[i+1], anc)
-                else:
-                        self.rccx(controls[i], controls[i+1], controls[i-1])
+                self.rccx(controls[i], controls[i+1], controls[i-1])
+        self.x(controls[0:2])
+        self.ccx(controls[0], controls[1], anc)
 
         self.x(clist)
 
--- frqi/test.py
+++ frqi/test.py
@@ -19,8 +19,9 @@ if __name__ == '__main__':
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         img_num = 0
 
+        testimg = [[255,255,255,255], [0, 0, 0, 0], [255, 255, 255, 255], [0, 0, 0, 0]]
         #show original image
-        plt.imshow(x_train[img_num], cmap='gray')
+        #plt.imshow(x_train[img_num], cmap='gray')
         #plt.savefig('mnistimg'+str(img_num)+'.png')
         #plt.show()
 
@@ -34,11 +35,13 @@ if __name__ == '__main__':
         qc = QuantumCircuit(q, anc, c)
 
         qc.frqiEncoder(x_train[img_num], q[0], q[1:qubit], anc[0])
+        #qc.frqiEncoder(testimg, q[0], q[1:qubit], anc[0])
 
         backend_sim = Aer.get_backend('qasm_simulator')
         shots = 1024000
 
         genimg = qc.frqiDecoder(x_train[img_num], backend_sim, shots, q[0], q[1:11], c)
+        #genimg = qc.frqiDecoder(testimg, backend_sim, shots, q[0], q[1:5], c)
         plt.imshow(genimg, cmap='gray', vmin=0, vmax=255)
         #plt.savefig('gen_'+str(img_num)+'.png')
         plt.show()
