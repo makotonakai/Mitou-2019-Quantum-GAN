f757174649eb4942621b4ae8159b36d469d4f63f
Nozomi Tanetani <nozomi@wifi-139-205.sfc.wide.ad.jp>
2020-02-07 20:12:29 +0900

    implement input for web app

--- web/qgan/quantum_gan/views.py
+++ web/qgan/quantum_gan/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .forms import ContactForm
 from django.http import HttpResponse
-from django.shortcuts import render
 import io
 import matplotlib.pyplot as plt
+import numpy as np
+
+image = []
 
 #png画像形式に変換数関数
 def plt2png():
@@ -18,12 +20,27 @@ def plt2png():
 
 # html表示view
 def index(request):
-    return render(request, 'qgan/index.html')
+    form = ContactForm()
+    hoge = {
+        'form': form,
+    }
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        print("post")
+        global image
+        image = []
+        #formで送信された値を取得
+        if form.is_valid():
+            print("VALIDATION SUCCESS")
+            for i in range(16):
+                image.append(int(form.cleaned_data['color'+str(i)]))
+            image = np.array(image)
+            image = image.reshape(4,4)
+            return render(request, 'qgan/result.html', {'form': form})
+    return render(request, 'qgan/index.html', hoge)
 
 #画像埋め込み用view
 def img_plot(request):
-    # matplotを使って作図する
-    image = [[255, 255],[0, 0]]
     plt.tick_params(labelbottom=False,labelleft=False,labelright=False,labeltop=False)
     plt.tick_params(bottom=False,left=False,right=False,top=False)
     plt.imshow(image, cmap='gray')
