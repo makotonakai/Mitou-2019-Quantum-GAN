00daed19d4ad8c1cc1d4e9aac5ce152f396ebe64
Nozomi Tanetani <nozomi@wifi-139-205.sfc.wide.ad.jp>
2020-02-07 20:14:12 +0900

    add form.py

---
+++ web/qgan/quantum_gan/forms.py
@@ -0,0 +1,131 @@
+from django import forms
+ 
+class ContactForm(forms.Form):
+    color0 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color1 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color2 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color3 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color4 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color5 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color6 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color7 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color8 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color9 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color10 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color11 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color12 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color13 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color14 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
+    color15 = forms.CharField(
+            required=True,
+            disabled=False,
+            max_length=3,
+            min_length=1,
+            widget=forms.TextInput(attrs={
+                'pattern':'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]',
+                'value':0}))
